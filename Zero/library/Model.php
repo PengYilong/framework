69124bf9e0f1778b49d942991ac5183b3d895d6c
Nezumi <948800401@qq.com>
2018-06-12 18:19:13 +0800

    the model which extends Model correlate the name of the table

--- Zero/library/Model.php
+++ Zero/library/Model.php
@@ -1,17 +1,35 @@
 <?php
 namespace Zero\library;
-
+use ReflectionClass;
 
 class Model
 {
 
+	/**
+	 * @var string prefix 
+	 */
+	protected $prefix;
+
+	/**
+	 * @var string name of table
+	 */
+	protected $table = NULL;
+	
 	protected $db; //a database instance
 
 	public function __construct()
 	{
+		$db_config = Config::get('database')['master'];
+		$this->prefix = $db_config['tablepre']; 
+		$this->table = $this->get_table_name();
 		$this->db = Factory::getDatabase();
 		$this->cache = Factory::getCache();
 	}			
 
-
+	public function get_table_name()
+	{
+		$sub_arr = explode('\\', get_class($this));
+		$sub_class = end($sub_arr);
+		return 	$this->prefix.to_underscore($sub_class);
+	}
 }
