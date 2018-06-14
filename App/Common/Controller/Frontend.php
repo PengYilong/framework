87d23539dfb2017f0b9112792cdafd28d6c86cfa
Nezumi <948800401@qq.com>
2018-06-15 03:12:42 +0800

    One module one style

--- App/Common/Controller/Frontend.php
+++ App/Common/Controller/Frontend.php
@@ -8,12 +8,12 @@ namespace App\Common\Controller;
 use Zero\library\Controller;
 use Zero\library\Factory;
 
-class Frontend extends Base
+class Frontend extends Controller
 {	
 	
 	public function __construct($module, $controller,$action)
 	{
+		$this->style = 'default';
 		parent::__construct($module, $controller,$action);
-		
 	}
 }
