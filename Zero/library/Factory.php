317f6dc3a143b8ef0f9d0d6670098793d28a28dc
Nezumi <948800401@qq.com>
2018-07-02 07:11:48 +0800

    Because model need to get name in real time,getModel function do not use Register Pattern

--- Zero/library/Factory.php
+++ Zero/library/Factory.php
@@ -17,21 +17,14 @@ class Factory
 
     public static function getModel($name, $type = 0)
     {
-       $typeName = $type ==0 ? 'model' : 'business';
-       $key = 'app_'.$typeName.'_'.$name;
-       $model = Register::get($key);
-       if(!$model){
-            $class = 'App\\'.self::$module.'\\'.ucwords($typeName).'\\'.$name;
-            if( self::$module ){
-                $model = new $class();
-                Register::set($key, $model);
-            } else {
-                throw new \Exception($model.' doesn\'t exist');
-            }
-            
-       }
+       $typeName = $type ===0 ? 'model' : 'business';
+        $class = 'App\\'.self::$module.'\\'.ucwords($typeName).'\\'.$name;
+        if( self::$module ){
+            $model = new $class();
+        } else {
+            throw new \Exception($model.' doesn\'t exist');
+        }
        return $model;
-       
     }
 
     public static function getDatabase( $id = 'master' )
